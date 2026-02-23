56dd87c592f1b2390a06b76597cb7a5470e211cf
Diego Cardona <160233987+diegocardonahernandez@users.noreply.github.com>
2026-02-23 23:34:26 +0100

    Redesign landing page; add user repository skeleton
    Completely overhauled Index.cshtml to create a custom Spanish-language landing page for SharpGains, featuring a hero section, features overview, call-to-action, and footer, all styled with Bootstrap. Added RepositoryUsuarios.cs with initial class structure and stored procedure reference for user registration, preparing for future user-related data operations.

---
+++ SharpGains/Repositories/RepositoryUsuarios.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using SharpGains.Data;
+using SharpGains.Models;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SharpGains.Repositories
+{
+    public class RepositoryUsuarios
+    {
+        #region STORED PROCEDURES
+        //REGISTRO DE USUARIO:
+
+        //        CREATE PROCEDURE SP_REGISTER_USER
+        //              (@nombre NVARCHAR(100),
+        //	            @apellidos NVARCHAR(150),
+        //	            @correo NVARCHAR(255),
+        //	            @contrasena NVARCHAR(MAX),
+        //	            @altura DECIMAL(5,2),
+        //	            @peso DECIMAL(5,2))
+        //      AS
+        //           DECLARE @idnuevousuario INT
+        //           SELECT @idnuevousuario = ISNULL(MAX(id), 0) + 1 FROM USUARIO
+
+        //          INSERT INTO USUARIO
+        //          VALUES(@idnuevousuario, @nombre, @apellidos, @correo, @contrasena, @altura, @peso)
+        //      GO
+        //      ------------------------
+        #endregion
+
+        private SharpGainsContext context;
+
+        public RepositoryUsuarios (SharpGainsContext context)
+        {
+            this.context = context;
+        }
+
+    }
+}
